9e7f3668593a65e5ea68b3b5d0868dc797882ed6
Johnny <risingvirtue@gmail.com>
2017-10-28 23:05:16 -0700

    fonts

--- javascript/myWebsite.js
+++ javascript/myWebsite.js
@@ -29,17 +29,17 @@ mapImage.src = "./img/map.png";
 function resetMap() {
 	ctx.clearRect(0,0, mapCanvas.width, mapCanvas.height);
 	ctx.drawImage(mapImage, 25, 50, 50, 50);
-	ctx.font = "30px Courier";
+	ctx.font = "25px Georgia";
 	ctx.fillStyle = "#000000";
-	ctx.fillText("Bear Maps", 100, 75);
+	ctx.fillText("Bear Maps", 125, 75);
 }
 
 mapCanvas.onmouseover = function onMouseover(e) {
 	ctx.clearRect(0,0, mapCanvas.width, mapCanvas.height);
 	ctx.drawImage(mapImage, 12.5, 37.5, 75, 75);
-	ctx.font = "30px Courier";
+	ctx.font = "25px Georgia";
 	ctx.fillStyle = "#F19CBB";
-	ctx.fillText("Bear Maps", 100, 75);
+	ctx.fillText("Bear Maps", 125, 75);
 }
 mapCanvas.addEventListener("mouseout",function() {
 	resetMap();
@@ -56,17 +56,19 @@ seasonImage.src = "./img/leaf1.png";
 function resetSeason() {
 	Sctx.clearRect(0,0, seasonCanvas.width, seasonCanvas.height);
 	Sctx.drawImage(seasonImage, 25, 50, 50, 50);
-	Sctx.font = "15px Courier";
+	Sctx.font = "25px Georgia";
 	Sctx.fillStyle = "#000000";
-	Sctx.fillText("Seasons Background", 100, 75);
+	Sctx.fillText("Seasons", 140, 75);
+	Sctx.fillText("Background", 120, 100);
 }
 
 seasonCanvas.onmouseover = function onMouseover(e) {
 	Sctx.clearRect(0,0, mapCanvas.width, mapCanvas.height);
 	Sctx.drawImage(seasonImage, 12.5, 37.5, 75, 75);
-	Sctx.font = "15px Courier";
+	Sctx.font = "25px Georgia";
 	Sctx.fillStyle = "#F19CBB";
-	Sctx.fillText("Seasons Background", 100, 75);
+	Sctx.fillText("Seasons", 140, 75);
+	Sctx.fillText("Background", 120, 100);
 }
 seasonCanvas.addEventListener("mouseout",function() {
 	resetSeason();
@@ -81,17 +83,19 @@ dataImage.src = "./img/database.png";
 function resetData() {
 	Dctx.clearRect(0,0, dataCanvas.width, dataCanvas.height);
 	Dctx.drawImage(dataImage, 25, 50, 50, 50);
-	Dctx.font = "15px Courier";
+	Dctx.font = "25px Georgia";
 	Dctx.fillStyle = "#000000";
-	Dctx.fillText("Database Development", 100, 75);
+	Dctx.fillText("Database", 140, 75);
+	Dctx.fillText("Development", 120, 100);
 }
 
 dataCanvas.onmouseover = function onMouseover(e) {
 	Dctx.clearRect(0,0, dataCanvas.width, dataCanvas.height);
 	Dctx.drawImage(dataImage, 12.5, 37.5, 75, 75);
-	Dctx.font = "15px Courier";
+	Dctx.font = "25px Georgia";
 	Dctx.fillStyle = "#F19CBB";
-	Dctx.fillText("Database Development", 100, 75);
+	Dctx.fillText("Database", 140, 75);
+	Dctx.fillText("Development", 120, 100);
 }
 dataCanvas.addEventListener("mouseout",function() {
 	resetData();
@@ -106,17 +110,17 @@ netImage.src = "./img/realNet1.png";
 function resetNet() {
 	Nctx.clearRect(0,0, dataCanvas.width, dataCanvas.height);
 	Nctx.drawImage(netImage, 25, 50, 50, 50);
-	Nctx.font = "30px Courier";
+	Nctx.font = "25px Georgia";
 	Nctx.fillStyle = "#000000";
-	Nctx.fillText("Bayes' Net", 100, 75);
+	Nctx.fillText("Bayes' Net", 125, 75);
 }
 
 netCanvas.onmouseover = function onMouseover(e) {
 	Nctx.clearRect(0,0, dataCanvas.width, dataCanvas.height);
 	Nctx.drawImage(netImage, 12.5, 37.5, 75, 75);
-	Nctx.font = "30px Courier";
+	Nctx.font = "25px Georgia";
 	Nctx.fillStyle = "#F19CBB";
-	Nctx.fillText("Bayes' Net", 100, 75);
+	Nctx.fillText("Bayes' Net", 125, 75);
 }
 netCanvas.addEventListener("mouseout",function() {
 	resetNet();

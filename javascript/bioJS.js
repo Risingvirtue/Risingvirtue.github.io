8a804ef531f576a9e73b98020424cd3167446201
Johnny <risingvirtue@gmail.com>
2017-11-08 01:18:16 -0800

    bio color template

--- javascript/bioJS.js
+++ javascript/bioJS.js
@@ -3,7 +3,23 @@ var ctx = canvas.getContext("2d");
 
 resizeCanvas();
 
-ctx.fillRect(0,0, canvas.width, canvas.height);
+function drawBackground() {
+	ctx.clearRect(0,0, canvas.width, canvas.height);
+	let gradient = ctx.createLinearGradient(0, canvas.height, canvas.width, 0);
+	gradient.addColorStop(0, '#6E352C');
+	gradient.addColorStop(1, '#F59A44');
+	ctx.fillStyle = gradient;
+	ctx.fillRect(0,0, canvas.width, canvas.height);
+}
+function update() {
+	requestAnimationFrame(update);
+	drawBackground();
+	ctx.font = "50px Georgia";
+	ctx. fillStyle = "#755330";
+	ctx.fillText("Bio", canvas.width / 2 - 50, canvas.height / 10);
+	ctx.clearRect(canvas.width / 4, canvas.height / 8, canvas.width / 2, 7 * canvas.height / 8);
+}
+
 //resize stack overflow
 function resizeCanvas(){
     if (canvas.width  != window.innerWidth) {
@@ -12,11 +28,7 @@ function resizeCanvas(){
     if (canvas.height != window.innerHeight) {
         canvas.height = window.innerHeight;
     }
-	let gradient = ctx.createLinearGradient(0, canvas.height, canvas.width, 0);
-	gradient.addColorStop(0, '#6E352C');
-	gradient.addColorStop(1, '#F59A44');
-	ctx.fillStyle = gradient;
-	ctx.fillRect(0,0, canvas.width, canvas.height);
+	update();
 	
 }
 window.addEventListener("resize", resizeCanvas);

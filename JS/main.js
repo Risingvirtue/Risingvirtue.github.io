7d055012f4a9225bd2990d2f0e98947cd41f46b9
Risingvirtue <risingvirtue@berkeley.edu>
2017-12-10 00:56:38 -0800

    email change

--- JS/main.js
+++ JS/main.js
@@ -1,14 +1,20 @@
-$("#submit").onclick(function() {
-	var email = $("#email").val();
-	var message = $("#message").val();
-	
-	if (email == "" || email == undefined) {
-		$(window).alert("Please input an email");
-	} else if (email) { //not valid email regex
-		alert("Invalid email");
-	} else if (message=="" || message == undefined) {
-		alert("Please enter a message");
-	} else {
-		//submit email to me
-	}
+$(document).ready(function() {
+	$("#myButton").click(function() {
+		var name = $("#name").val();
+		var phone = $("#phone").val();
+		var message = $("#message").val();
+		var phoneReg = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/;
+		//alert("he");
+		if (name == "" || name == undefined) {
+			alert("Please enter a valid name");
+		} else if (!phone.match(phoneReg)) {
+			alert("Please enter a valid phone number");
+		} else {
+			var subject = name + " is interested in your work!";
+			message += " You can contact me through my phone number: " + phone + ".";
+			window.open('mailto:johnnyon@berkeley.edu?subject=' + subject + '&body=' + message);
+		}
+		
+		
+	});
 });
